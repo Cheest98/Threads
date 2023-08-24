78978191648974d6ffe89fd185814902017a42f7
Cheest98 <listwanlukasz08@gmail.com>
2023-08-24 18:15:12 +0200

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+// <UserButton afterSignOutUrl="/" />
+export default function Home() {
+  return (
+    <div>
+      <h1 className="head-text text-left"> </h1>
+    </div>
+  );
+}
