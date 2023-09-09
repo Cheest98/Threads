bb7c0362283d2f6abe13a603982fe70d3caaeb45
Cheest98 <listwanlukasz08@gmail.com>
2023-09-09 14:28:23 +0200

    thread comments

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
-import { currentUser } from "@clerk/nextjs";
-import { fetchPosts } from "@/lib/actions/thread.actions";
 import ThreadCard from "@/components/cards/ThreadCard";
+import { fetchPosts } from "@/lib/actions/thread.actions";
+import { currentUser } from "@clerk/nextjs";
 
 export default async function Home() {
   const result = await fetchPosts(1, 30);
